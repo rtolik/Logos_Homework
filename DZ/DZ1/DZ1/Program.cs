00af3e9432f3de930d301d77fbba0aaae0dd0afd
Anatoliy <govier999@gmail.com>
2016-11-16 12:35:43 +0200

    Made homework for lesson1

---
+++ DZ/DZ1/DZ1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choise;
+            Console.WriteLine("Please enter number of homework:");
+            Console.WriteLine("1)Averange mark for 3 subjects");
+            Console.WriteLine("2)Length of circle ");
+            choise=int.Parse(Console.ReadLine());
+            switch (choise)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Please enter mark for first subject");
+                        int mark1, mark2, mark3;
+                        mark1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Please enter mark for second subject");
+                        mark2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Please enter mark for third subject");
+                        mark3 = int.Parse(Console.ReadLine());
+                        float aver;
+                        aver = (float)(mark1 + mark2 + mark3) / 3;
+                        Console.WriteLine("Averange mark for this subjects is " + aver);
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine("Please enter radius of circle");
+                        int rad = int.Parse(Console.ReadLine());
+                        float dov =(float)(System.Math.Pow(rad, 2) * 3.14);
+                        Console.WriteLine("Length of circle is " + dov);
+                    }
+                    break;
+            }
+
+        }
+    }
+}
